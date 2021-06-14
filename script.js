d0660ffbb0cf15bdf81d9654133d1195e11148bd
Nicole Cox <nicoleanalisecox@gmail.com>
2021-06-14 20:12:56 +0100

    base styling and structure

---
+++ script.js
@@ -0,0 +1,20 @@
+var css = document.querySelector("h3");
+var colour1 = document.querySelector(".colour1");
+var colour2 = document.querySelector(".colour2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ colour1.value 
+	+ ", " 
+	+ colour2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+setGradient()
+
+colour1.addEventListener("input", setGradient);
+
+colour2.addEventListener("input", setGradient);
