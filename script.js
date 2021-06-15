e3abbf4190a5ed9325090afc7d2d7c2d4485187b
Nicole Cox <nicoleanalisecox@gmail.com>
2021-06-15 20:24:22 +0100

    add ability to set gradient direction

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-var css = document.querySelector(".cssTextDisplay");
+// ELEMENT SELECTIONS
+// for colour inputs
 var colour1 = document.querySelector(".colour1");
 var colour2 = document.querySelector(".colour2");
 var body = document.getElementById("gradient");
-var copyBtn = document.querySelector(".copyBtn")
+// for text output
+var css = document.querySelector(".cssTextDisplay");
+// for copy button
+var copyBtn = document.querySelector(".copyBtn");
+// for gradient direction arrows
+var upBtn = document.querySelector("#upBtn");
+var leftBtn = document.querySelector("#leftBtn");
+var rightBtn = document.querySelector("#rightBtn");
+var downBtn = document.querySelector("#downBtn");
 
+// for gradient direction settings
+var direction = "right";
+
+// MAIN
 // check whether Clipboard API available in the browser
 // if not available, hide copy button
 if (!navigator.clipboard) {
 	copyBtn.classList.add('.hidden');
 }
 
+// sets gradient according to colour picker inputs & updates css export text
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ colour1.value 
-	+ ", " 
-	+ colour2.value 
-	+ ")";
+	body.style.background =
+		"linear-gradient(to " +
+		direction + "," +
+		colour1.value +
+		", " +
+		colour2.value +
+		")";
 
 	css.value = body.style.background + ";";
 
@@ -36,7 +51,28 @@ function copyToClipboard() {
 
 setGradient()
 
+// EVENT LISTENERS
+// for colour inputs
 colour1.addEventListener("input", setGradient);
 colour2.addEventListener("input", setGradient);
 
+// for copy button
 copyBtn.addEventListener("click", copyToClipboard);
+
+// for gradient direction buttons
+leftBtn.addEventListener("click", () => {
+	direction = "left";
+	setGradient();
+})
+rightBtn.addEventListener("click", () => {
+	direction = "right";
+	setGradient();
+})
+upBtn.addEventListener("click", () => {
+	direction = "top";
+	setGradient();
+})
+downBtn.addEventListener("click", () => {
+	direction = "bottom";
+	setGradient();
+})
