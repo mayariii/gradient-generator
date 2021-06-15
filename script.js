61d2ef29e25d501f0f1b64f1ea1f0bb8fc5af3e1
Nicole Cox <nicoleanalisecox@gmail.com>
2021-06-15 19:35:33 +0100

    add copy to clipboard feature

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-var css = document.querySelector("h3");
+var css = document.querySelector(".cssTextDisplay");
 var colour1 = document.querySelector(".colour1");
 var colour2 = document.querySelector(".colour2");
 var body = document.getElementById("gradient");
+var copyBtn = document.querySelector(".copyBtn")
+
+// check whether Clipboard API available in the browser
+// if not available, hide copy button
+if (!navigator.clipboard) {
+	copyBtn.classList.add('.hidden');
+}
 
 function setGradient() {
 	body.style.background = 
@@ -11,10 +18,25 @@ function setGradient() {
 	+ colour2.value 
 	+ ")";
 
-	css.textContent = body.style.background + ";";
+	css.value = body.style.background + ";";
+
+	// clear styling of copy button
+	copyBtn.innerHTML = '<i class="far fa-copy copyIcon fa-lg"></i> Copy To Clipboard';
+	copyBtn.classList.remove('copied');
 }
+
+function copyToClipboard() {
+	css.select();
+	document.execCommand("copy");
+
+	// add styling to confirm copy
+	copyBtn.innerHTML = '<i class="fas fa-check"></i> Copied!'
+	copyBtn.classList.add('copied');
+}
+
 setGradient()
 
 colour1.addEventListener("input", setGradient);
-
 colour2.addEventListener("input", setGradient);
+
+copyBtn.addEventListener("click", copyToClipboard);
